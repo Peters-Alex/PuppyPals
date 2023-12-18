8d6641aa316086e68f6dd783d389e7fb547aab86
Alex Peters <mr.peters@Alexs-MacBook-Air-2.local>
2023-12-17 20:38:10 -0600

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {puppyList} from './data.js'
 import './App.css'
+import './index.css'
 import {useState} from 'react'
+ 
+
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
